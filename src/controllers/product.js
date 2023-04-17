085091ad4f82c010408864cd7f01e770d8d81f6f
hiep <phanhuyhiep2k3@gmail.com>
2023-04-17 09:08:26 +0700

    update validate product

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,9 +1,19 @@
 
 import Product from "../models/product"
+import joi from "joi"
 
-
+const productSchema = joi.object({
+    name: joi.string().required(),
+    price: joi.number().required()
+})
 export const create = async (req, res) => {
-    try {        
+    try { 
+        const {error} = productSchema.validate(req.body);
+        if(error) {
+            res.json({
+                message: error.details[0].message,
+            })
+        }       
         const product = await Product.create(req.body);
         return res.status(201).json({
             massage: "Tạo sản phẩm thành công",
@@ -40,6 +50,12 @@ export const get = async (req, res) => {
 
 export const update = async (req, res) => {
     try {
+        const {error} = productSchema.validate(req.body);
+        if(error) {
+            res.json({
+                message: error.details[0].message,
+            })
+        }  
         const products = await Product.findOneAndUpdate({_id: req.params.id}, req.body, {new: true});
         return res.status(201).json({
             massage: "Cập nhật sản phẩm thành công",
@@ -53,7 +69,14 @@ export const update = async (req, res) => {
 }
 
 export const remove = async (req, res) => {
+
     try {
+        const {error} = productSchema.validate(req.body);
+        if(error) {
+            res.json({
+                message: error.details[0].message,
+            })
+        }  
         const products = await Product.findOneAndDelete({_id: req.params.id});
         return res.status(201).json({
             massage: "Xóa sản phẩm thành công",
